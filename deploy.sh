b2f64d0c55d40612e8e780dac36be42fc65afafb
Jason Wittenbach <jason.wittenbach@gmail.com>
2019-07-27 12:32:37 -0400

    updates submodule from https://github.com/jwittenbach/home/commits/866fd2b1c23c1e6162c89fa063ffeddcf90d5c15

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ message=$1
 hexo clean; hexo generate
 
 git add .
-git commit -m $message
-commit_hash=`git log --oneline | awk 'NR==1{print $1}'`
+git commit -m \"$message\"
+commit_hash=`git log --oneline | awk 'NR==1{print $1}' | xargs git rev-parse`
 commit_url=https://github.com/jwittenbach/home/commits/$commit_hash
 
 rm -rf jwittenbach.github.io/*
---
+++ source/_posts/testing-deplot-script.md
@@ -0,0 +1,5 @@
+---
+title: testing deplot script
+date: 2019-07-27 12:31:52
+tags:
+---
