3b0f5cf3e35db88f4f5208f10eda0386662c60ee
Jason Wittenbach <jason.wittenbach@gmail.com>
2019-07-27 12:00:08 -0400

    updates submodule from https://github.com/jwittenbach/home/commits/d41ca7c

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,8 @@ commit_url=https://github.com/jwittenbach/home/commits/$commit_hash
 
 rm -rf jwittenbach.github.io/*
 cp -R public/* jwittenbach.github.io/
-git submodule foreach git commit -a -m "updates content from $commit_url"
+git submodule foreach git add .
+git submodule foreach git commit -m "updates content from $commit_url"
 git submodule foreach push
 
 git add jwittenbach.github.io
